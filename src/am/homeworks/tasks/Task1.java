9a88e66f486321dcb373d7978945283d2e3e3cd7
Martirosyan Sevak <sevak.martirosyan@ihg.com>
2018-05-16 15:48:29 +0400

    created task 1 carcase.

---
+++ src/am/homeworks/tasks/Task1.java
@@ -0,0 +1,23 @@
+package am.homeworks.tasks;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int firstNumber;
+        int secondNumber;
+        int thirdNumber;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter first number: ");
+        firstNumber= scanner.nextInt();
+        System.out.println("Please enter second number: ");
+        secondNumber= scanner.nextInt();
+        System.out.println("Please enter third number: ");
+        thirdNumber= scanner.nextInt();
+
+//        ստեղ արդեն լոգիկան կգրես  էտ ստացած թվերի հետ, նույն ձևի մնացած սաղ թասկերը
+
+    }
+}
